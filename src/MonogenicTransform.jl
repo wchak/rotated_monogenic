284d634efa0d614078606b42f0e05279854ce0f2
wchak <wchak@ucdavis.edu>
2023-12-01 15:50:02 -0800

    Update MonogenicTransform.jl

--- src/MonogenicTransform.jl
+++ src/MonogenicTransform.jl
@@ -10,23 +10,6 @@ struct GaussianHP <: MonoFilterTypes end
 struct GaussianLP <: MonoFilterTypes end
 
 
-function monogenic_filter(RHO; Monotype = GaussianHP())
-    
-    if typeof(Monotype) == GaussianLP
-        # For Guassian Low Pass Filter
-        LP  =  exp.( -(RHO.^2)/8 );       # Gaussian low-pass filter
-        HP  =  sqrt.( 1 .- LP.^2 );       # Complementary high-pass filter
-        
-    elseif typeof(Monotype) == GaussianHP
-        # For Guassian High Pass Filter
-        HP  =  1 .- exp.( -(RHO.^2)/2 );  # Gaussian-based high-pass filter
-        LP  =  sqrt.( 1 .- HP.^2 );       # Complementary low-pass
-    end
-    
-    return LP, HP
-    
-end
-
 function (Mono::MonoConvFFT)(x::AbstractArray{<:Number,4})
 
 # input: (nx, ny, nchannels, nexamples)
